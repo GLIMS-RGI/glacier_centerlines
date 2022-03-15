b934ff02b5b908ee56f507b46c8ef50a018ef4c0
franra9 <francescrouradserias@gmail.com>
2022-03-15 13:00:52 +0100

    clean code

--- heads.py
+++ heads.py
@@ -55,18 +55,6 @@ if plot:
 #log = logging.getLogger(__name__)
 #logging.basicConfig(filename="test.log", level=logging.my_DEBUG)
 
-#some parameters:
-# Kienholz et al eq (1)
-q1 = 2e-6
-q2 = 500. #m
-rmax = 1000. #m
-
-# Kienholz et al eq (2)
-f1 = 1000.
-f2 = 3000.
-a = 4.25
-b = 3.7  
-
 single_fl = False
 
 # get altitude and pixel info: 
@@ -83,16 +71,8 @@ for i in np.arange(len(crop_extent)):
     dem_clipped0 = dem_clipped.copy()
 
     # assign some value to outside crop: e.g. 0 (default number is too large)
-    dummy_val = dem_clipped.values[0][dem_clipped.values[0] < 1500].mean()
-    dem_clipped.values[0][dem_clipped.values[0] > 1500] = dummy_val
-    #dem_clipped.values[0][10][10:20] = 0
-    #dem_clipped.values[0][9][10:20] = 0
-
-    #if plot:
-      #  f, ax = plt.subplots(figsize=(8, 10))
-      #  dem_clipped.plot(ax=ax)
-      #  ax.set(title="Raster Layer Cropped to Geodataframe Extent")
-      #  plt.show()
+    dummy_val = dem_clipped.values[0][dem_clipped.values[0] < 32000].mean()
+    dem_clipped.values[0][dem_clipped.values[0] > 32000] = dummy_val
 
 #plot zoutline
     area = crop_extent.geometry[i].area
@@ -107,12 +87,6 @@ for i in np.arange(len(crop_extent)):
     # get terminus coordinates and position in or data (index)
     xyterm, ind_term = get_terminus_coord(points_yx, prof[1]) #yx, zoutline
 
-    # if plot:
-    #     #plt.plot(prof[0], prof[1]) 
-    #     plt.plot(prof[0], prof[1], 'o-') #horizontal distance vs altitude
-    #     plt.plot(prof[0][ind_term], prof[1][ind_term], 'r*') #terminus
-    
-    #     plt.show()   
     zoutline = prof[1]
     ext_yx = points_yx
     
@@ -134,7 +108,7 @@ for i in np.arange(len(crop_extent)):
     ### I dont undersatnd this -dx instead of - dy ????
     grid = salem.Grid(proj=utm_proj, nxny=(nx, ny), dxdy=(dx, -dx), x0y0=x0y0) 
     
-    localmax_window = 500 #I don't know units. It has to be in units of dx
+    localmax_window = 500 #In units of dx
     
     # Size of the half window to use to look for local maxima
     maxorder = np.rint(localmax_window/dx) #np.rint(cfg.PARAMS['localmax_window'] / gdir.grid.dx)
@@ -171,10 +145,10 @@ for i in np.arange(len(crop_extent)):
     heads = [shpg.Point(x, y) for y, x in zip(headsy,
                                               headsx)]
     
-#    # get radius of the buffer according to Kienholz eq. (1)
-#    q1 = 2/10**6 # 1/m
-#    q2 = 500 #m
-#    rmax = 1000 #m
+    # get radius of the buffer according to Kienholz eq. (1)
+    q1 = 2/10**6 # 1/m
+    q2 = 500 #m
+    rmax = 1000 #m
     
     radius = q1 * area + q2 # cfg.PARAMS['q1'] * geom['polygon_area'] + cfg.PARAMS['q2']
     radius = utils.clip_scalar(radius, 0, rmax) #cfg.PARAMS['rmax'])
@@ -188,21 +162,14 @@ for i in np.arange(len(crop_extent)):
     
     # Plus our criteria, quite useful to remove short lines:
     radius += flowline_junction_pix * flowline_dx #cfg.PARAMS['flowline_junction_pix'] * cfg.PARAMS['flowline_dx']
-    #logging.debug('(%s) radius in raster coordinates: %.2f', radius)
-    #print('(%s) radius in raster coordinates: %.2f', radius)
     
     # OK. Filter and see.
-    #logging.debug('(%s) number of heads before radius filter: %d', len(heads))
-    #print('(%s) number of heads before radius filter: %d', len(heads))
     poly_pix = crop_extent.geometry[i]
     
     #for r=500 it does the job in the last glacier
     heads, heads_z = _filter_heads(heads, list(heads_z), float(radius), poly_pix)
-    #logging.debug('(%s) number of heads after radius filter: %d', len(heads))
-    #print('(%s) number of heads after radius filter: %d', len(heads))
     
     #if some head removed:
-        
     headsx=np.zeros(1)
     headsy=np.zeros(1)
     for k in np.arange(len(heads)):
@@ -226,57 +193,28 @@ for i in np.arange(len(crop_extent)):
     ext = ext_yx
     z = dem_clipped.values[0]
     
- #   x_ind = np.zeros(1)
- #   y_ind = np.zeros(1)
- #   points = []
-    
- #   x_ind = x_ind.astype(int)
- #   y_ind = y_ind.astype(int)
-    
- #   for p in np.arange(1,len(ext)): 
- #       x_ind = np.append(x_ind, int(np.where(dem_clipped0.x == \
- #                                   find_nearest(dem_clipped0.x, ext[p][0]))[0][0]))
- #       y_ind = np.append(y_ind, int(np.where(dem_clipped0.y == \
- #                                   find_nearest(dem_clipped0.y, ext[p][1]))[0][0]))
- #       dem_clipped0.values[0][1,5]
-#        x_ind = np.where(dem_clipped0.x == find_nearest(dem_clipped0.x, ext[p][1]))[0][0]
-#        y_ind = np.where(dem_clipped0.y == find_nearest(dem_clipped0.y, ext[p][1]))[0][0]
-#    dem_clipped0.values[0][y_ind,x_ind] = 2    
-    
 #points = dem_clipped0[0, [y_ind], [x_ind]]
     glacier_poly_hr = crp1.geometry[i]
     proj=utm_proj
-    
-#     ###problemmmm!!!
-#  ####   glacier_poly_hr = shapely.ops.transform(proj, glacier_poly_hr)
- #   if not glacier_poly_hr.is_valid:
- #         raise InvalidGeometryError('This glacier geometry is not valid.')
 
-     # # Rounded nearest pix
+# Rounded nearest pix
     glacier_poly_pix = _polygon_to_pix(glacier_poly_hr)
-#    if glacier_poly_pix.exterior is None:
-#        raise InvalidGeometryError('Problem in converting glacier geometry '
-#                                     'to grid resolution.')
 
     from functools import partial, wraps
 
     tuple2int = partial(np.array, dtype=np.int64)
 
     # Compute the glacier mask (currently: center pixels + touched)
-    #nx, ny = gdir.grid.nx, gdir.grid.ny
     glacier_mask = mask.values[0]
-    #glacier_mask = np.zeros((ny, nx), dtype=np.uint8)
     glacier_ext = np.zeros((ny, nx), dtype=np.uint8)
     (x, y) = glacier_poly_pix.exterior.xy
-    #glacier_mask[skdraw.polygon(np.array(y), np.array(x))] = 1
     
     for gint in glacier_poly_pix.interiors:
          x, y = tuple2int(gint.xy)
-         #glacier_mask[skdraw.polygon(y, x)] = 0
          xx, yy = grid.transform(x,y,crs=utm_proj)
          xx, yy = np.round(xx), np.round(yy)
          xx, yy = xx.astype(int), yy.astype(int)
-         glacier_mask[yy, xx] = 0  # on the nunataks, no
+         glacier_mask[yy, xx] = 0  # on the nunataks
     
     x, y = tuple2int(glacier_poly_pix.exterior.xy)
     
