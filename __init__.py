1047676c96858aeff72ab850f312a2c4b85f489b
franra9 <francescrouradserias@gmail.com>
2023-02-14 21:18:44 +0100

    change init to make in pip installable

--- __init__.py
+++ __init__.py
@@ -1,2 +1,13 @@
 # This is a dummy init.
 __version__ = '1.0.1'
+
+# API
+# Some decorators used by many
+from oggm.utils import entity_task, global_task
+
+#add functions
+from glacier_centerlines import functions_rgi
+
+# Classes
+from glacier_centerlines.centerlines_rgi import compute_centerlines_rgi
+
